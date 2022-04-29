6d8a1302f3776129b3e967d7f7d50f9d3991faeb
Albert Montagut Casero <albert.montagut.casero@gmail.com>
2022-04-29 19:19:51 +0200

    feat: domain service locator

---
+++ Assets/Project/Code/Domain/Interactor/DomainServiceLocator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asteroids
+{
+    public class DomainServiceLocator
+    {
+        private static DomainServiceLocator instance;
+
+        public static DomainServiceLocator GetInstance()
+        {
+            return instance ??= new DomainServiceLocator();
+        }
+
+        private readonly Dictionary<Type, Service> serviceDictionary;
+
+        private DomainServiceLocator()
+        {
+            this.serviceDictionary = new Dictionary<Type, Service>();
+        }
+        
+        public void RegisterController<T>(T service) where T: Service
+        {
+            Type type = typeof(T);
+            this.serviceDictionary.Add(type, service);
+        }
+
+        public T GetService<T>()
+        {
+            Type type = typeof(T);
+
+            return (T) this.serviceDictionary[type];
+        }
+
+        public void UnRegisterService<T>()
+        {
+            Type type = typeof(T);
+            this.serviceDictionary.Remove(type);
+        }
+        
+        public void Clear()
+        {
+            this.serviceDictionary.Clear();
+        }
+    }
+}
