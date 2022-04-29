42b1787550ad1dc84ddb002ca283bacc793ddfeb
Albert Montagut Casero <albert.montagut.casero@gmail.com>
2022-04-29 20:29:15 +0200

    feat: gameplay installer updated and all systems correctly wired

---
+++ Assets/Project/Code/GameplayInstaller.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class GameplayInstaller: MonoBehaviour
+    {
+        [SerializeField]
+        private InputController inputControllerPrefab;
+        
+        [SerializeField]
+        private MovementComponent playerPrefab;
+
+        [SerializeField] 
+        private AsteroidsCamera asteroidsCameraPrefab;
+
+        private InputController inputController;
+        private MovementComponent player;
+
+        private void Start()
+        {
+            this.player = Instantiate(this.playerPrefab);
+            var asteroidsCamera = Instantiate(this.asteroidsCameraPrefab);
+            var mapSizeProvider = new ToroidalMapProvider(asteroidsCamera);
+            var map = mapSizeProvider.GetMap();
+            
+            var domainEventDispatcher = new DomainSignalDispatcher();
+            
+            var toroidalMovementUseCase = new ToroidalMovementUseCase(map, domainEventDispatcher);
+            DomainServiceLocator.GetInstance().RegisterController(toroidalMovementUseCase);
+
+            this.inputController = Instantiate(this.inputControllerPrefab);
+            this.inputController.SetMovementComponent(this.player);
+        }
+    }
+}
+
+
+
